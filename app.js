c91fdbd5c0221f616d04ac393c5f16c27c57f1a2
tomegz <tomaszbubala@o2.pl>
2018-04-04 19:08:58 +0200

    add helpers to every request & templates

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const routes = require("./routes/index");
+const helpers = require("./helpers");
 
 // Express app
 const app = express();
@@ -9,6 +10,15 @@ const app = express();
 // Turn raw request into useful properties on req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Pass variables to our templates + all requests
+app.use((req, res, next) => {
+  res.locals.h = helpers;
+  res.locals.flashes = req.flash();
+  res.locals.user = req.user || null;
+  res.locals.currentPath = req.path;
+  next();
+});
 
 // Views setup
 app.set("views", path.join(__dirname, "views"));
